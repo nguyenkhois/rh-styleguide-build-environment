51308fe7a78596cfe4c698e4cc2356108ce26039
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-07-10 07:48:29 +0200

    Modified the Sample component

--- src/components/Sample/Sample.js
+++ src/components/Sample/Sample.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import './style.scss';
+import * as Handlebars from '../../helpers/handlebars.min.js';
 
-/* ------ Component building ------ */
-const ComponentContent = () => (
-    <div class="sample">
-        <h1 class="sample__title">The page content - New</h1>
-        <p class="sample__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac felis sit amet augue ultricies condimentum. Aliquam sagittis ligula quis erat consectetur, vel auctor enim tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur in cursus nulla. Proin vitae eleifend libero. Cras neque sapien, vulputate venenatis pellentesque quis, pretium vitae nibh. Fusce placerat arcu ut tortor elementum, vel rutrum ex condimentum.
-        </p>
-    </div>
-);
-/* ------ End of Component building ------ */
-
+/* Component including */
+import './sample.scss';
+import { context } from './sample.config.json';
+import hbsSource from './sample.hbs';
+const hbsTemplate = Handlebars.compile(hbsSource);
 
 export class Sample extends Component {
     render() {
         return (
             <React.Fragment>
-                <ComponentContent />
+                <span dangerouslySetInnerHTML={{ __html: hbsTemplate(context) }} />
             </React.Fragment>
         );
     }
